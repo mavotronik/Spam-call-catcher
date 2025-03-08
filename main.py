42cb34ccc427356f070ce22a906a17f5545edaae
Mavotronik <matveypopov2004@gmail.com>
2025-03-09 01:08:27 +0300

    some multithreading added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import serial
 import time
+import threading
+import re
 
 # Serial port settings
 SERIAL_PORT = "/dev/ttyACM0"
@@ -11,6 +13,27 @@ def send_at_command(ser, command, delay=1):
     time.sleep(delay)
     response = ser.read_all().decode(errors="ignore")  # Parce input
     return response.strip()
+
+def monitor_signal_strengh(ser):
+    # Request for signal strength every 60 seconds
+    while True:
+        response = send_at_command(ser, "AT+CSQ")
+        print("[INFO] Signal:", response)
+        time.sleep(60)
+
+def wait_for_incomig_call(ser):
+    while True:
+        while True:
+            if ser.in_waiting > 0:  # Check if there is new data
+                response = ser.read_all().decode(errors="ignore").strip()
+            if "RING" in response:
+                print("[ALERT] Incoming call detected!")
+            match = re.search(r'\+CLIP: "(\d+)"', response)
+            if match:
+                caller_number = match.group(1)
+                print(f"[ALERT] Incoming call from: {caller_number}")
+            time.sleep(2)  # Check every 2 seconds
+
 
 # Open TTY
 with serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=2) as ser:
@@ -45,6 +68,18 @@ with serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=2) as ser:
     response = send_at_command(ser, "AT+COPS?")
     print("[INFO] Operator:", response)
 
-    # Request for signal strength
-    response = send_at_command(ser, "AT+CSQ")
-    print("[INFO] Signal:", response)
+   # Enable caller ID notification
+    send_at_command(ser, "AT+CLIP=1")
+    print("[INFO] Caller ID detection enabled") 
+
+    signal_thread = threading.Thread(target=monitor_signal_strengh, args=(ser,))
+    signal_thread.daemon = True
+    signal_thread.start()
+
+    signal_thread = threading.Thread(target=wait_for_incomig_call, args=(ser,))
+    signal_thread.daemon = True
+    signal_thread.start()
+ 
+
+    while True:
+        time.sleep(2)
