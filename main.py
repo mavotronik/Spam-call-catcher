3766ce477e45dddf9cb20076e0e4c81d21984640
Mavotronik <matveypopov2004@gmail.com>
2025-03-11 20:22:20 +0300

    add requirements.txt

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ config = load_config()
 
 gprs = GPRS()
 
+gprs.initialize()
 gprs.test_echo()
 gprs.call_detect()
 
@@ -20,3 +21,23 @@ def monitor_signal():
         signal = gprs.get_signal_strength()
         if signal is not None:
             print(f"[INFO] Signal strength: {signal}")
+
+def monitor_calls():
+    while True:
+        data = gprs.read_data()
+        if data:
+            if "RING" in data:
+                print("[ALERT] Incoming call detected!")
+                return True
+            if "+CLIP:" in data:
+                caller_number = data.split('"')[1]
+                print(f"[ALERT] Incoming call from: {caller_number}")
+        time.sleep(2)
+
+threading.Thread(target=monitor_signal, daemon=True).start()
+threading.Thread(target=monitor_calls, daemon=True).start()
+
+# Основной цикл
+while True:
+
+    time.sleep(1)
